40f1600e11f42a754f520e675b78c50a45b3aa34
symunona <symunona@gmail.com>
2018-03-01 13:09:21 -0500

    tesla reader

--- tesla/parser.js
+++ tesla/parser.js
@@ -8,33 +8,22 @@ const jolicitron = require("jolicitron")
 
 module.exports = function parse(rawData){
 
-    // This is an example parser for the Youtube example.
-
-    // 1.- Describe the file
-    // 2.- Parse it
-    // The rest will be taken care of by bostich
-
-    // Note: if you made a mistake, the "remaining" field
-    // of the output will not be an empty string.
 
     let parser = jolicitron((save, n) =>[
-        save('videoCount'),
-        save('endpointCount'),
-        save('requestDescriptionCount'),
-            'cacheServerCount',
-            'cacheServerSize',
-        n('videoSizes', {length: 'videoCount', indicies: true}),
-        n('endpoints', {length: 'endpointCount'},
-            'toDataServerLatency',
-            save('connectedCacheServerCount'),
-            n('cacheServerLatencies', {length: 'connectedCacheServerCount'}, 
-            'cacheServerId', 
-            'latency')            
-        ),        
-        n('requests', {length: 'requestDescriptionCount'}, 
-            'videoId', 
-            'endpointId', 
-            'requestCount')
+        'cityHeight',
+        'cityWidth',
+        'carCount',
+        save('rideCount'),
+        'bonus',
+        'maxSteps',                
+        n('rides', {length: 'rideCount', indicies: true}, 
+            'fromY', 
+            'fromX', 
+            'toY', 
+            'toX', 
+            'start',
+            'finish'
+        )        
     ])
 
     return parser(rawData)
